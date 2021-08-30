bb5e519772614d30f7a2e47dec8b903891449d90
Johnny <johnnylabedzki@hotmail.com>
2021-08-30 12:24:30 -0700

    Rendered the urlDatabase in express_server.js into the urls_index.ejs file.

--- express_server.js
+++ express_server.js
@@ -16,18 +16,12 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-})
 
 app.get('/urls', (req, res) => {
   const templateVariables = {urls: urlDatabase};
   res.render('urls_index', templateVariables);
 })
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
