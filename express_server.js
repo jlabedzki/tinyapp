1ca6195526dd7d3e77fa2ced72f227be51438e9e
Johnny <johnnylabedzki@hotmail.com>
2021-08-30 13:18:03 -0700

    Added a post request handler to express_server.js, implemented a function to generate a random string (6 characters) long, and created the template for the url input form.

--- express_server.js
+++ express_server.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = () => {
+  return (Math.random() + 1).toString(36).substring(6);
+};
+
+const string = generateRandomString();
+console.log(string);
 
 const urlDatabase = {
   'b2xBn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.use(morgan('dev'));
 
 
 app.get('/urls', (req, res) => {
   const templateVariables = {urls: urlDatabase};
   res.render('urls_index', templateVariables);
-})
+});
+
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
@@ -27,6 +42,12 @@ app.get('/urls/:shortURL', (req, res) => {
   }
   res.render('urls_show', templateVariables);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 
 
 app.listen(PORT, () => {
