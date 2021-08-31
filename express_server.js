4a2c3c9d203a6506b4998bfbfeea379cafaa4930
Johnny <johnnylabedzki@hotmail.com>
2021-08-30 19:15:46 -0700

    Added a login/logout feature by tracking a username cookie.

--- express_server.js
+++ express_server.js
@@ -87,6 +87,13 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/logout', (req, res) => {
+  const username = Object.keys(req.cookies)[0];
+  
+  res.clearCookie(username);
+  res.redirect('/urls');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
