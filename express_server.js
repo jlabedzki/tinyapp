a005741c33eb309a0453e277808796b017eff212
Johnny <johnnylabedzki@hotmail.com>
2021-08-30 20:30:02 -0700

    Added a register page with an email and password form intake. Changed the cookie to track a user_id instead of username, and then created a user profile object with a random user id, their email, and their password.

--- express_server.js
+++ express_server.js
@@ -18,12 +18,17 @@ const urlDatabase = {
   '923sdf': "http://www.lighthouselabs.ca"
 };
 
+const users = {};
 
 //URL index template
 app.get('/urls', (req, res) => {
   const templateVariables = {
     urls: urlDatabase,
-    username: req.cookies['username']
+    user: {
+      id: req.cookies['user_id'],
+      email: users[req.cookies['user_id']].email,
+      password: users[req.cookies['user_id']].password
+    }
   };
   res.render('urls_index', templateVariables);
 });
@@ -31,7 +36,11 @@ app.get('/urls', (req, res) => {
 //New URL template
 app.get('/urls/new', (req, res) => {
   const templateVariables = {
-    username: req.cookies['username']
+    user: {
+      id: req.cookies['user_id'],
+      email: users[req.cookies['user_id']].email,
+      password: users[req.cookies['user_id']].password
+    }
   }
 
   res.render('urls_new', templateVariables);
@@ -44,17 +53,28 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVariables = {
     shortURL,
     longURL: urlDatabase[shortURL],
-    username: req.cookies["username"]
+    user: {
+      id: req.cookies['user_id'],
+      email: users[req.cookies['user_id']].email,
+      password: users[req.cookies['user_id']].password
+    }
   }
   res.render('urls_show', templateVariables);
 });
 
+//Register template
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+//Generate a shortURL, add it to our url database, and then redirect to url_shows.ejs
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL
   res.redirect(`/urls/${shortURL}`);
 });
 
+//Redirect to the original URL that client provided when creating shortURL
 app.get('/u/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
 
@@ -63,12 +83,14 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
+//Delete a shortURL
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
 
+//Edit a shortURL to link to a different longURL
 app.post('/urls/:shortURL', (req, res) => {
   let shortURL;
   for (const short in urlDatabase) {
@@ -81,16 +103,35 @@ app.post('/urls/:shortURL', (req, res) => {
   res.redirect('/urls');
 });
 
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username);
+// //Track username after login submission via cookie
+// app.post('/login', (req, res) => {
+//   const username = req.body.username;
+//   res.cookie('username', username);
+//   res.redirect('/urls');
+// });
+
+//Clear cookie after logout
+app.post('/logout', (req, res) => {
+  const username = Object.keys(req.cookies['user_id']);
+  console.log(username);
+  
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
-app.post('/logout', (req, res) => {
-  const username = Object.keys(req.cookies)[0];
-  
-  res.clearCookie(username);
+//Generate user profile and track the user id with a cookie.
+app.post('/register', (req, res) => {
+  const randomUserID = generateRandomString();
+
+  users[randomUserID] = {
+    id: randomUserID,
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  console.log(users);
+
+  res.cookie('user_id', randomUserID);
   res.redirect('/urls');
 });
 
