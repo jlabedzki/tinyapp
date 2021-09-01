c680a59f188833adae820bcc00a3ff1626c6cb2b
Johnny <johnnylabedzki@hotmail.com>
2021-08-31 18:09:39 -0700

    Final touch ups before project submission. Need to still create a README.md and waiting on a mentor code review.

--- express_server.js
+++ express_server.js
@@ -25,9 +25,11 @@ const users = {};
 
 //Redirect to url_index page or login page based on whether or not the user is logged in.
 app.get('/', (req, res) => {
+
   if (users[req.session.user_id] === undefined) {
     res.redirect('/login');
   }
+
   res.redirect('/urls');
 })
 
@@ -39,6 +41,7 @@ app.get('/urls', (req, res) => {
     urls: urlDatabase,
     user: users[req.session.user_id]
   };
+
   res.render('urls_index', templateVariables);
 });
 
@@ -46,48 +49,54 @@ app.get('/urls', (req, res) => {
 
 //New URL template
 app.get('/urls/new', (req, res) => {
+
   const templateVariables = {
     user: users[req.session.user_id]
   }
 
   if (users[req.session.user_id] === undefined) {
-    console.log('Must be logged in');
     res.redirect('/login');
-  } else {
-    res.render('urls_new', templateVariables);
   }
+
+  res.render('urls_new', templateVariables);
 });
 
 
 
 //Show URLs template
 app.get('/urls/:shortURL', (req, res) => {
+
   const shortURL = req.params.shortURL;
 
   //If the short URL doesn't exist, then we give a 404 statuscode
   //If the user tries to view a URL that they didn't create, then we give a 403 status code
   if (!urlDatabase[shortURL]) {
     res.redirect(404, '/urls');
-  } else if (urlDatabase[shortURL].userID !== req.session.user_id) {
-    res.redirect(403, '/urls');
-  } else {
-    const templateVariables = {
-      shortURL,
-      longURL: urlDatabase[shortURL].longURL,
-      user: users[req.session.user_id]
-    }
-
-    res.render('urls_show', templateVariables);
   }
+
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
+    res.redirect(403, '/urls');
+  }
+
+  const templateVariables = {
+    shortURL,
+    longURL: urlDatabase[shortURL].longURL,
+    user: users[req.session.user_id]
+  }
+
+  res.render('urls_show', templateVariables);
 });
 
 
 
 //Register template
 app.get('/register', (req, res) => {
+
+  //If user is logged in, redirect them to the /urls page
   if (req.session.user_id) {
     res.redirect('/urls');
   }
+
   res.render('register');
 });
 
@@ -95,9 +104,11 @@ app.get('/register', (req, res) => {
 
 //Login template
 app.get('/login', (req, res) => {
+
   if (req.session.user_id) {
     res.redirect('/urls');
   }
+
   res.render('login');
 });
 
@@ -107,25 +118,26 @@ app.get('/login', (req, res) => {
 app.post('/urls', (req, res) => {
 
   //If the user is not logged in, redirect them to login page before creating a shortened URL
-
-  //Once the user is logged in, we add the short URL to our urldatabase object as a key, with a object value containing the respective long URL as well as the ID of the user that created the short URL
   if (users[req.session.user_id] === undefined) {
     res.redirect('/login');
-  } else {
-    const shortURL = generateRandomString();
-    urlDatabase[shortURL] = {
-      longURL: req.body.longURL,
-      userID: req.session.user_id
-    }
-
-    res.redirect(`/urls/${shortURL}`);
   }
+
+  //Once the user is logged in, we add the short URL to our urldatabase object as a key, with a object value containing the respective long URL as well as the ID of the user that created the short URL
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id
+  }
+
+  res.redirect(`/urls/${shortURL}`);
+
 });
 
 
 
 //Redirect to the original URL that client provided when creating shortURL
 app.get('/u/:shortURL', (req, res) => {
+
   const shortURL = req.params.shortURL;
 
   //If the url doesn't exist in the urldatabase, then we give a 404 statuscode
@@ -142,18 +154,21 @@ app.get('/u/:shortURL', (req, res) => {
 
 //Delete a shortURL
 app.post('/urls/:shortURL/delete', (req, res) => {
+
   const shortURL = req.params.shortURL;
 
   //If the shortURl doesn't exist, then we give a 404 statuscode
   //If the user is trying to delete a shortURL that they didn't create, then we give a 403 statuscode
   if (!urlDatabase[shortURL]) {
     res.redirect(404, 'back');
-  } else if (urlDatabase[shortURL].userID !== req.session.user_id) {
-    res.redirect(403, 'back');
-  } else {
-    delete urlDatabase[shortURL];
-    res.redirect('/urls');
   }
+
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
+    res.redirect(403, 'back');
+  }
+
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
 });
 
 
@@ -163,16 +178,17 @@ app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
 
   //If the shortURL is not found, then we give a 404 statuscode
-  //If the user is trying to edit a shortURL that they didn't create, then we give a 403 status code
   if (!urlDatabase[shortURL]) {
     res.redirect(404, 'back');
-  } else if (urlDatabase[shortURL].userID !== req.session.user_id) {
-    res.redirect(403, 'back');
-  } else {
-
-    urlDatabase[shortURL].longURL = req.body.longURL;
-    res.redirect('/urls');
   }
+
+  //If the user is trying to edit a shortURL that they didn't create, then we give a 403 status code
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
+    res.redirect(403, 'back');
+  }
+
+  urlDatabase[shortURL].longURL = req.body.longURL;
+  res.redirect('/urls');
 });
 
 
@@ -185,8 +201,10 @@ app.post('/register', (req, res) => {
     res.redirect(400, 'back');
   }
 
-  //If the email entered matches an existing account's email address, then we give a 400 status code
+  //Use helper function to find a userID by email
   const user = getUserByEmail(req.body.email, users);
+
+  //If the email entered matches an existing account's email address, then we give a 400 status code
   if (user) {
     res.redirect(400, 'back');
   }
@@ -220,12 +238,14 @@ app.post('/login', (req, res) => {
   let passwordMatch = false;
   let randomUserID;
 
-  //If the email provided matches an email in the users object, then we set email match to true and then check the password
-  //If the password matches, then we set the randomUserId to the id of the user profile that matched
+  //Use helper function to find a userID based on email
   const user = getUserByEmail(req.body.email, users);
+
+  //If the email provided matches an email in the users database, then we set email match to true and then check the password
   if (user) {
     emailMatch = true;
 
+    //If the password matches, then we set the randomUserId to the id of the user profile that matched
     if (bcrypt.compareSync(req.body.password, users[user].password)) {
       passwordMatch = true;
       randomUserID = user;
@@ -237,9 +257,10 @@ app.post('/login', (req, res) => {
   if (emailMatch === true && passwordMatch === true) {
     req.session.user_id = randomUserID;
     res.redirect('/urls');
-  } else {
-    res.redirect(403, 'back');
   }
+
+  //If the password and email do not match to a user in the database, then we give a 403 status code
+  res.redirect(403, 'back');
 });
 
 
@@ -252,7 +273,5 @@ app.post('/logout', (req, res) => {
 
 
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+app.listen(PORT);
 
